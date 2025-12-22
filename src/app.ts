d581f00fc0b194fa5b2069839a56b036fff896ba
asifikbal09 <asif.ikbal662@gmail.com>
2025-12-22 23:56:07 +0600

    style(app): format code for consistency and improve response structure

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,22 @@ import cors from "cors";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import router from "./app/router";
 
-const app:Application = express();
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send({message:"Welcome to your dream flat."})
-})
+app.get("/", (req, res) => {
+  res.sendStatus(200).send({
+    success: true,
+    statusCode: 200,
+    message: "Welcome to your dream flat.",
+  });
+});
 
-app.use("/api",router)
+app.use("/api", router);
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
